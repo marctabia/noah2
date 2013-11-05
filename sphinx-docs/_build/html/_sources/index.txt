ecc4df38823a5146e0ec05c6b816d5e0e0b9885d
Marc Tabia <marctabia@gmail.com>
2013-11-05 15:12:02 +0800

    Changed title for Documentation

--- sphinx-docs/_build/html/_sources/index.txt
+++ sphinx-docs/_build/html/_sources/index.txt
@@ -3,7 +3,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to NOAH-WebDocs's documentation!
+Welcome to NOAH-WebDocs Documentation!
 ========================================
 
 Contents:
--- sphinx-docs/_build/html/searchindex.js
+++ sphinx-docs/_build/html/searchindex.js
@@ -1,1 +1,1 @@
-Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,noah:0,modul:0,indic:0,content:0,tabl:0,document:0,page:0,webdoc:0},objtypes:{},titles:["Welcome to NOAH-WebDocs&#8217;s documentation!"],objnames:{},filenames:["index"]})
+Search.setIndex({objects:{},terms:{index:0,search:0,welcom:0,noah:0,modul:0,indic:0,content:0,tabl:0,document:0,page:0,webdoc:0},objtypes:{},titles:["Welcome to NOAH-WebDocs Documentation!"],objnames:{},filenames:["index"]})
